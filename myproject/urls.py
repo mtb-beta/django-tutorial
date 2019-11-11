831e55b7d10cdf3d298882c7c0c7b6bad10d0afb
mtb_beta <mtb.toya0403@gmail.com>
2019-11-11 20:08:43 +0700

    initial commmit

---
+++ myproject/urls.py
@@ -0,0 +1,35 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+from accounts import views as account_views
+from boards import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^signup/$', account_views.signup, name='signup'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^reset/$',
+        auth_views.PassordResetView.as_view(
+            template_name='password_reset.html',
+            email_template_name='password_reset_email.html',
+            subject_template_name='passord_reset_subject.txt'
+        ),
+        name='password_reset'),
+    url(r'^reset/done/$',
+        auth_views.PasswordRestDoneView.as_view(
+            template_name='password_reset_done.html'),
+        name='password_reset_done'
+    ),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1, 20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    url(r'^reset/complete/$',
+        auth_view.PasswordResetCompleteView.as_view(template_name='password_reset_complte.html'),
+        name='password_reset_complete'
+    ),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
+    url(r'^admin/', admin.site.urls),
+]
