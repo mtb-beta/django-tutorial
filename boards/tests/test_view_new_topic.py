234569794fd659fc9c170d9eaeac37008968c77f
mtb_beta <mtb.toya0403@gmail.com>
2019-11-19 00:51:09 +0700

    2019/11/19の成果

--- boards/tests/test_view_new_topic.py
+++ boards/tests/test_view_new_topic.py
@@ -1,52 +1,23 @@
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve
 
-from boards.views import home, board_topics, new_topic
+from boards.views import new_topic
 from boards.models import Board, Topic, Post
 from boards.forms import NewTopicForm
 
-class HomeTest(TestCase):
+class LoginRequiredNewTopicTests(TestCase):
     def setUp(self):
-        self.board = Board.objects.create(name="Django", description="Django board")
-        url = reverse('home')
-        self.response = self.client.get(url)
+        Board.objects.create(name="Django", description="Django board.")
+        self.url = reverse('new_topic', kwargs={'pk': 1})
+        self.response = self.client.get(self.url)
 
-    def test_home_view_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_home_url_resolver_home_view(self):
-        view = resolve('/')
-        self.assertEquals(view.func, home)
-
-    def test_home_view_contains_lint_to_topics_page(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
-        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
-
-class BoardTopicsTests(TestCase):
-    def setUp(self):
-        Board.objects.create(name='Django', description="Django board.")
-
-    def test_board_topics_view_success_status_code(self):
-        url = reverse('board_topics', kwargs={'pk': 1})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
-
-    def test_board_topics_view_now_found_status_code(self):
-        url = reverse('board_topics', kwargs={'pk': 99})
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, 404)
-
-    def test_board_topics_url_resolves_board_topics_view(self):
-        view = resolve('/boards/1/')
-        self.assertEquals(view.func, board_topics)
-
-    def test_board_topics_view_contains_link_back_to_homepage(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
-        response = self.client.get(board_topics_url)
-        homepage_url = reverse('home')
-        self.assertContains(response, 'href="{0}"'.format(homepage_url))
+    def test_redirection(self):
+        login_url = reverse('login')
+        self.assertRedirects(self.response, '{login_url}?next={url}'.format(
+            login_url=login_url, url=self.url)
+        )
 
 
 class NewTopicTests(TestCase):
@@ -57,12 +28,12 @@ class NewTopicTests(TestCase):
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_new_topic_view_not_found_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 99})
         response = self.client.get(url)
-        self.assertEquals(response.status_code, 404)
+        self.assertEqual(response.status_code, 404)
 
     def test_new_topic_url_resolves_new_topic_view(self):
         view = resolve('/boards/1/new/')
@@ -105,7 +76,7 @@ class NewTopicTests(TestCase):
             'message': ''
         }
         response = self.client.post(url, data)
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
 
@@ -120,5 +91,5 @@ class NewTopicTests(TestCase):
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
         form = response.context.get('form')
-        self.assertEquals(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         self.assertTrue(form.errors)
--- boards/views.py
+++ boards/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import Http404
-from .forms import NewTopicForm
-from .models import Board, Topic, Post
+
+from boards.forms import NewTopicForm
+from boards.models import Board, Topic, Post
 
 def home(request):
     boards = Board.objects.all()
@@ -15,6 +17,7 @@ def board_topics(request, pk):
         raise Http404
     return render(request, 'topics.html', {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()
@@ -24,12 +27,12 @@ def new_topic(request, pk):
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
             post = Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', pk=board.pk)
     else:
